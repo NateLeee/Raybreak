fb1ccb6a8760d7463d60e440f6d6c993d50a7c60
Nate Lee <nateleee.dev@gmail.com>
2020-07-12 16:23:53 +0800

    It's running on my real device now, which is a good signal.

--- Raybreak/ViewController.swift
+++ Raybreak/ViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
+import MetalKit
 
 class ViewController: UIViewController {
 
+    var metalView: MTKView {
+        return view as! MTKView
+    }
+    
+    var device: MTLDevice!
+    var commandQueue: MTLCommandQueue!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        metalView.device = MTLCreateSystemDefaultDevice()
+        device = metalView.device
+        
+        metalView.clearColor = MTLClearColor(red: 0, green: 0.4, blue: 0.21, alpha: 0.9)
+        commandQueue = device.makeCommandQueue()
+        
+        let commandBuffer = commandQueue.makeCommandBuffer()
+        let commandEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: metalView.currentRenderPassDescriptor!)
+        
+        commandEncoder?.endEncoding()
+        commandBuffer?.present(metalView.currentDrawable!)
+        commandBuffer?.commit()
+        
     }
 
 
 }
 
+extension ViewController: MTKViewDelegate {
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+        
+    }
+    
+    func draw(in view: MTKView) {
+        
+    }
+    
+    
+}
